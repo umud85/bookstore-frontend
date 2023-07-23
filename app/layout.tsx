96f24f1a4e335929a4a00eed16f1244ce1f60a0c
Umut Akbayin <umutakbayin@posteo.de>
2023-07-23 21:08:11 +0200

    add menubar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Menubar from "@/components/Menubar";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Menubar />
+        {children}
+      </body>
     </html>
   )
 }
---
+++ components/Menubar.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+
+export default function ButtonAppBar() {
+  const menuitems = ["Home", "Books", "About"]
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{
+              display: { md: "none" },
+              mr: 2
+            }}
+          >
+            <MenuIcon sx={{ display: { md: "none" }, }} />
+          </IconButton>
+          <Typography sx={{ display: {md: "block" }}}>The Bookstore</Typography>
+          <List sx={{ display: { xs: "none", md: "flex" }}}>
+            {menuitems.map((item) => (
+              <ListItem key={item}>
+                <ListItemText primary={item} />
+            </ListItem>
+            ))}
+          </List>
+          <Button sx={{ marginLeft: "auto" }} color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
