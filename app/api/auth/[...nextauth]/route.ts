1844494d73cd9dae00f8bb0846f27a286e0fa378
Umut Akbayin <umutakbayin@posteo.de>
2023-07-31 22:16:04 +0200

    change setup for default login

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,7 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github"
+import { options } from "./options";
 
 
-const handler = NextAuth({
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
-  ],
-})
+const handler = NextAuth(options);
 
 export { handler as GET, handler as POST }
--- app/books/signedin/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function SignedIn() {
-  return <p>You are signed in.</p>
-}
