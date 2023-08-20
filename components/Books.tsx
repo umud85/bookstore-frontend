6665cf9c2bfa8e91f0fa1c90702b6388c9a0dbfd
Umut Akbayin <umutakbayin@posteo.de>
2023-08-20 22:43:24 +0200

    add github authentication

--- components/Books.tsx
+++ components/Books.tsx
@@ -1,12 +1,26 @@
+"use client";
+
 import { Book } from "../types/interfaces";
+import { useSession } from "next-auth/react";
 
 
-export default function Books({ books }: { books: Book[] }) {
+export default function Books({ books }: { books: Book[]; }) {
+  const { data: session, status } = useSession();
+
+  if (status === "authenticated") {
+    return (
+      <section>
+        <ul>
+          {books.map((book) => <li key={book.id.timestamp}>{book.title}</li>)}
+        </ul>
+      </section>
+    );
+  }
+
   return (
-    <section>
-      <ul>
-        {books.map((book) => <li key={book.id.timestamp}>{book.title}</li>)}
-      </ul>
-    </section>
+    <>
+      <p>You are not signed in.</p>
+      <a href="/api/auth/signin">Sign in</a>
+      </>
   );
 }
